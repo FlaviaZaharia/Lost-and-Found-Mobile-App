e7b01dcf8fb5fe8b21bff6e532db03c09c5a71c5
Flavia <flaviazaharia@yahoo.com>
2021-12-06 21:38:01 +0200

    Layout fixes

--- app/src/main/java/com/app/lostandfound/LoginActivity.java
+++ app/src/main/java/com/app/lostandfound/LoginActivity.java
@@ -3,10 +3,13 @@ package com.app.lostandfound;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.SpannableString;
 import android.text.TextUtils;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,6 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginActivity extends AppCompatActivity {
     private EditText emailTextView,passwordTextView;
     private Button b;
+    private TextView message,errorTextView;
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +32,24 @@ public class LoginActivity extends AppCompatActivity {
         mAuth=FirebaseAuth.getInstance();
         emailTextView=findViewById(R.id.email);
         passwordTextView=findViewById(R.id.password);
+        errorTextView=findViewById(R.id.error);
         b=findViewById(R.id.login);
-
+        message=findViewById(R.id.loginToRegister);
+        SpannableString content = new SpannableString(message.getText().toString());
+        content.setSpan(new UnderlineSpan(), 0, message.length(), 0);
+        message.setText(content);
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 loginUser();
             }
         }
         );
+        message.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     private void loginUser(){
@@ -43,7 +57,7 @@ public class LoginActivity extends AppCompatActivity {
         email=emailTextView.getText().toString();
         password=passwordTextView.getText().toString();
         if(TextUtils.isEmpty(email)||TextUtils.isEmpty(password)){
-            Toast.makeText(getApplicationContext(),"Email and password cannot be empty",Toast.LENGTH_LONG ).show();
+            errorTextView.setText("Please fill in all the fields");
         return;
         }
         mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(
--- app/src/main/java/com/app/lostandfound/MainActivity.java
+++ app/src/main/java/com/app/lostandfound/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     private static int TIME_OUT = 3000;
