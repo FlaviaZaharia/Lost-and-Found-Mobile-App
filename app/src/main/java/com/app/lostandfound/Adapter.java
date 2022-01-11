7f78a23be5aadb9c07a2298ceb46ea7c689e7837
GabrielaZarnescu <zarnescu_gabriela@yahoo.com>
2022-01-12 01:31:14 +0200

    idk

--- app/src/main/java/com/app/lostandfound/Adapter.java
+++ app/src/main/java/com/app/lostandfound/Adapter.java
@@ -1,70 +1,44 @@
 package com.app.lostandfound;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.Nullable;
 
-import org.w3c.dom.Text;
+import com.squareup.picasso.Picasso;
 
-import java.util.List;
+import java.util.ArrayList;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-    private List<PostModel> posts;
-    public Adapter(List<PostModel> posts) {
-        this.posts=posts;
-    }
-
-    public void setPosts(List<PostModel> posts) {
-        this.posts = posts;
+public class Adapter extends ArrayAdapter<PostModel> {
+    public Adapter(@NonNull Context context, ArrayList<PostModel> postModelArrayList) {
+        super(context, 0, postModelArrayList);
     }
 
     @NonNull
     @Override
-    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.post_design,parent,false);
-        return new ViewHolder(view); }
-
-    @Override
-    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
-
-        int resource = posts.get(position).getImage();
-        String title=posts.get(position).getTitle();
-        String description=posts.get(position).getDescription();
-        String category=posts.get(position).getCategory();
-
-        holder.setData(resource,title,description,category);
-    }
-    @Override
-    public int getItemCount() {
-        return posts.size();
-    }
-
-    //holder class
-    public class ViewHolder extends RecyclerView.ViewHolder {
-
-        private ImageView img;
-        private TextView ttl;
-        private TextView desc;
-        private TextView categ;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            img=itemView.findViewById(R.id.imageView1);
-            ttl=itemView.findViewById(R.id.titlePost);
-            desc=itemView.findViewById(R.id.description);
-            categ=itemView.findViewById(R.id.category);
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listitemView = convertView;
+        if (listitemView == null) {
+            // Layout Inflater inflates each item to be displayed in GridView.
+            listitemView = LayoutInflater.from(getContext()).inflate(R.layout.post_design, parent, false);
         }
-        public void setData(int res,String title,String description,String category) {
-            img.setImageResource(res);
-            ttl.setText(title);
-            desc.setText(description);
-            categ.setText(category);
-        }
+        PostModel postModel = getItem(position);
+        TextView title = listitemView.findViewById(R.id.titlePost);
+        title.setText(postModel.getTitle());
+        TextView category = listitemView.findViewById(R.id.categoryPost);
+        category.setText(postModel.getCategory());
+        TextView desc = listitemView.findViewById(R.id.descriptionPost);
+        desc.setText(postModel.getDescription());
+        ImageView image = listitemView.findViewById(R.id.imageView1);
+        Picasso.get().load(postModel.getImage()).into(image);
 
+
+        return listitemView;
     }
 }
